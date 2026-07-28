79d24b3a23d8e449316a8de973f6fa64f9f2765c
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2026-07-28 15:05:39 -0400

    Type hinting change for added specificity

--- entrypoint.py
+++ entrypoint.py
@@ -3,18 +3,18 @@
 import io
 from typing import Any
 
-from fdk import response
+from fdk.response import Response
 
 from modules.app import run_update
 
 
-def handler(ctx: Any, data: io.BytesIO | None = None) -> Any:
+def handler(ctx: Any, data: io.BytesIO | None = None) -> Response:
     status_code, response_data = run_update()
 
-    return response.Response(ctx,
-                             status_code=status_code,
-                             response_data=response_data,
-                             headers={'Content-Type': 'text/plain'})
+    return Response(ctx,
+                    status_code=status_code,
+                    response_data=response_data,
+                    headers={'Content-Type': 'text/plain'})
 
 def main() -> int:
     status_code, response_data = run_update()
